bb911cc803e3b04dd18eab7413d7b396ba1a70c2
Trieu Tran <114438848+trieut415@users.noreply.github.com>
2024-01-06 14:30:18 -0800

    Create StringMatching.h

---
+++ StringMatching.h
@@ -0,0 +1,16 @@
+#ifndef StringMatching_h
+#define StringMatching_h
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <cctype>
+
+using namespace std;
+
+int StringMatching();
+
+#endif /* StringMatching_h */
